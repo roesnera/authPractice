f30893a09ddb1a5bef96bbe2e143b39f48b76e52
roesnera <adam.roesner@generation.org>
2023-05-28 14:01:57 -0400

    completes initial filtering

---
+++ src/main/java/com/example/authpractice/config/ApplicationConfig.java
@@ -0,0 +1,23 @@
+package com.example.authpractice.config;
+
+import com.example.authpractice.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+@Configuration
+@RequiredArgsConstructor
+public class ApplicationConfig {
+
+    private final UserRepository userRepository;
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> userRepository.findByEmail(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User not found"));
+    }
+
+}
